b9543b7da8d0d64baf613f62916c44a181b3d42e
Mohammed CHAHBOUN <m97.chahboun@gmail.com>
2022-03-19 23:41:15 +0100

    solved bugs caused by rename objects

--- lib/src/mc_view.dart
+++ lib/src/mc_view.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mc/src/mc_model.dart';
-
 import 'mc_constants.dart';
 import 'mc_exception.dart';
 import 'mc_llistenable.dart';
