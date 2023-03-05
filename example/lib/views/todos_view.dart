68913928d39c14a1775ca73321b554fe3667aabc
Mohammed <m97.chahboun@gmail.com>
2023-03-05 16:34:37 +0100

    Added todos view

--- example/lib/views/todos_view.dart
+++ example/lib/views/todos_view.dart
@@ -1,36 +1,49 @@
-import 'package:example/models/todos.dart';
-import 'package:example/requests/todos.dart';
+import 'package:example/models/todo.dart';
+import 'package:example/requests/todos_request.dart';
 import 'package:flutter/material.dart';
 import 'package:mvc_rocket/mvc_rocket.dart';
 
-class TodosExample extends StatelessWidget {
-  TodosExample({Key? key, required this.title}) : super(key: key);
+class TodosView extends StatelessWidget {
+  TodosView({Key? key, required this.title}) : super(key: key);
   final String title;
-
-  final todos = Todos();
+  final todoModel = Rocket.add("todos", Todos(), readOnly: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: RocketView(
-        model: todos,
-        call: () => GetTodos.gettodos(todos),
-        builder: (context) {
-          return ListView.builder(
-            itemCount: todos.multi!.length,
-            itemBuilder: (context, int index) {
-              final todo = todos.multi![index];
-              return CheckboxListTile(
-                title: Text(todo.title!),
-                value: todo.completed,
-                onChanged: (value) {
-                  todo.updateFields(completedField: value);
-                },
-              );
-            },
-          );
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          todoModel.all!.insert(0, Todos(title: "", completed: false));
+          todoModel.rebuildWidget();
         },
       ),
+      body: RocketView(
+          model: todoModel,
+          call: () => GetTodos.getTodos(todoModel),
+          callType: CallType.callIfModelEmpty,
+          builder: (context) {
+            return ListView.builder(
+              itemCount: todoModel.all!.length,
+              itemBuilder: (context, index) {
+                final todo = todoModel.all![index];
+                return CheckboxListTile(
+                  value: todo.completed,
+                  title: TextField(
+                    controller: TextEditingController(text: todo.title!),
+                    onChanged: (value) {
+                      todo.title = value;
+                    },
+                  ),
+                  onChanged: (value) {
+                    todo.updateFields(completedField: value);
+                  },
+                );
+              },
+            );
+          }),
     );
   }
 }
