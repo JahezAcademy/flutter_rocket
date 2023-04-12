99b2c3ad78153056abac3b7d1a045fbbbe0de874
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2023-04-12 14:50:12 +0000

    Removed mapToString method from request (#44)
    * Removed mapToString method and use builtin method instead
    
    * format codes

--- lib/src/mc_request.dart
+++ lib/src/mc_request.dart
@@ -125,17 +125,6 @@ class RocketRequest {
 
   static _onError(Object e) => log(e.toString());
 
-  //DONE: rename to maptoParams & inject into Map
-  @protected
-  String _mapToString(Map mp) {
-    String result = "";
-    mp.forEach((key, value) {
-      String and = mp.keys.last != key ? "&" : "";
-      result = "${result + key}=$value$and";
-    });
-    return result;
-  }
-
   /// دالة خاصة لجلب البيانات على شكل (قاموس)
   ///
   /// Json=>(قاموس)
@@ -161,7 +150,7 @@ class RocketRequest {
       model.state = RocketState.loading;
     }
     StreamedResponse? response;
-    String mapToParams = params != null ? _mapToString(params) : "";
+    String mapToParams = Uri(queryParameters: params ?? {}).query;
     Uri url = Uri.parse("${this.url}/$endpoint?$mapToParams");
     Request request = Request(method.name, url);
     request.body = json.encode(data);
