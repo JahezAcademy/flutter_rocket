0a0158889d61a5f9f9553e96c9c5af263ddf8c44
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2023-06-12 08:25:23 -0700

    Split request process methods (#52)
    * Split process methods and remove debugging param
    
    * updated sdk
    
    * Fixed minor depc & usd new switch statement
    
    * updated http package
    
    * Fixed type issue
    
    * removed unused code

--- lib/src/mc_request.dart
+++ lib/src/mc_request.dart
@@ -12,7 +12,6 @@ class RocketRequest {
   final String url;
   final Map<String, String> headers;
   final bool setCookies;
-  final bool debugging;
 
   /// انشاء الطلب.
   ///
@@ -38,103 +37,80 @@ class RocketRequest {
   /// [setCookies]
   ///
   /// Cookies تفعيل او الغاء حاصية الحفاظ على
-  ///
-  /// [debugging]
-  ///
-  /// console تفعيل او الغاء ظهور المشاكل في
-  RocketRequest(
-      {required this.url,
-      this.headers = const {},
-      this.setCookies = false,
-      this.debugging = true});
 
-  _getDebugging(String response, int statusCode, String? endpoint) {
-    if (debugging) {
-      log("\x1B[38;5;2m ########## mc package ########## \x1B[0m");
-      log("\x1B[38;5;2m [Url] => ${"$url/${endpoint!}"} \x1B[0m");
-      log("\x1B[38;5;2m [Response] => ${response.toString()} \x1B[0m");
-      log("\x1B[38;5;2m [$statusCode] => ${msgByStatusCode(statusCode)} \x1B[0m");
-      log("\x1B[38;5;2m ################################ \x1B[0m");
-    }
-  }
-
-  String msgByStatusCode(int statusCode) {
-    switch (statusCode) {
-      case 400:
-        return "the request entity sent by your application could not be understood by the server due to malformed syntax (e.g. invalid payload, data type mismatch)";
-      case 401:
-        return "there is a problem with the credentials provided by your application. This code indicates that your application tried to operate on a protected resource without providing the proper authorization. It may have provided the wrong credentials or none at all";
-      case 403:
-        return "your application is not authorized to access the requested resource, or when your application is being rate limited";
-      case 404:
-        return "the resource requested by your application does not exist";
-      case 405:
-        return "the HTTP method used by your application is not allowed for the resource";
-      case 406:
-        return "the resource requested by your application is not capable of generating response entities that are compliant the Accept headers sent";
-      case 408:
-        return "your application did not produce a request within the time that the server was prepared to wait";
-      case 409:
-        return "the request sent by your application could not be completed due to a conflict with the current state of the resource";
-      case 415:
-        return "the request entity sent by your application is in a format not supported by the requested resource for the requested method";
-      case 429:
-        return "your application has made too many calls and has exceeded the rate limit for this service";
-      case 500:
-        return "the server encountered an unexpected condition which prevented it from fulfilling the request sent by your application";
-      case 502:
-        return "the server encountered an unexpected condition which prevented it from fulfilling the request sent by your application";
-      case 503:
-        return "the server is currently unable to handle the request sent by your application due to a temporary overloading or maintenance of the server";
-      case 504:
-        return "the server, while acting as a gateway or proxy, did not get a response in time from the upstream server that it needed in order to complete the API call.";
-      default:
-        return "unknown";
-    }
-  }
+  RocketRequest({
+    required this.url,
+    this.headers = const {},
+    this.setCookies = false,
+  });
 
   @protected
-  dynamic _processData<T>(StreamedResponse response,
-      {RocketModel<T>? model,
+  Future<RocketModel> _processModel<T>(StreamedResponse response,
+      {required RocketModel<T> model,
       Function(dynamic data)? inspect,
       List<String>? targetData,
       String? endpoint}) async {
     // TODO(M97chahboun) : Refactor conditions
-    var result = json.decode(utf8.decode(await response.stream.toBytes()));
-    if (response.statusCode < 300 && response.statusCode >= 200) {
-      if (inspect != null) {
-        result = inspect(result);
-      } else if (targetData != null) {
-        try {
-          result = _getTarget(result, targetData);
-        } catch (e) {
-          log("Error in Target : $e, Try to use inspect instead");
-        }
-      }
-      if (model != null) {
+    String respDecoded = utf8.decode(await response.stream.toBytes());
+    switch (response.statusCode) {
+      case < 300 && >= 200:
+        var result = json.decode(respDecoded);
+        result = _handleTarget(inspect, result, targetData);
         if (result is List?) {
           model.setMulti(result ?? []);
-          return model.all;
         } else {
           model.fromJson(result);
-          return model;
         }
-      }
-      return result;
-    } else {
-      if (model != null) {
+      default:
         model.setException(RocketException(
-          response: result.toString(),
+          response: respDecoded,
           statusCode: response.statusCode,
         ));
-      }
-      _onError(RocketException(
-        response: result.toString(),
-        statusCode: response.statusCode,
-      ));
-      _getDebugging(result.toString(), response.statusCode, endpoint);
-      return result;
+        _onError(RocketException(
+          response: respDecoded,
+          statusCode: response.statusCode,
+        ));
     }
+    return model;
+  }
+
+  _processData<T>(StreamedResponse response,
+      {Function(dynamic data)? inspect,
+      List<String>? targetData,
+      String? endpoint}) async {
+    String respDecoded = utf8.decode(await response.stream.toBytes());
+    switch (response.statusCode) {
+      case < 300 && >= 200:
+        var result = json.decode(respDecoded);
+        result = _handleTarget(inspect, result, targetData);
+        return result;
+      default:
+        _onError(RocketException(
+          response: respDecoded,
+          statusCode: response.statusCode,
+        ));
+        late dynamic result = respDecoded;
+        try {
+          result = json.decode(respDecoded);
+        } catch (e) {
+          result = respDecoded;
+        }
+        return result;
+    }
+  }
+
+  _handleTarget(
+      Function(dynamic data)? inspect, result, List<String>? targetData) {
+    if (inspect != null) {
+      result = inspect(result);
+    } else if (targetData != null) {
+      try {
+        result = _getTarget(result, targetData);
+      } catch (e) {
+        log("Error in Target : $e, Try to use inspect instead");
+      }
+    }
+    return result;
   }
 
   static _onError(Object e) => log(e.toString());
@@ -175,11 +151,16 @@ class RocketRequest {
       if (setCookies) {
         _updateCookie(response);
       }
-      return _processData<T>(response,
-          model: model,
-          inspect: inspect,
-          endpoint: endpoint,
-          targetData: targetData);
+      if (model != null) {
+        return _processModel<T>(response,
+            model: model,
+            inspect: inspect,
+            endpoint: endpoint,
+            targetData: targetData);
+      } else {
+        return _processData<T>(response,
+            inspect: inspect, endpoint: endpoint, targetData: targetData);
+      }
     } catch (error, stackTrace) {
       return _catchError(error, stackTrace, response, model: model);
     }
