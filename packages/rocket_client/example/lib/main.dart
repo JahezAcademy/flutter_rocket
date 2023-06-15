de6dad09adedcbc74c07a6f9115322dd54063af8
Mohammed <m97.chahboun@gmail.com>
2023-06-15 12:52:25 +0100

    minorr update on rocket client example

--- packages/rocket_client/example/lib/main.dart
+++ packages/rocket_client/example/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rocket Client Example',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -38,7 +38,7 @@ class RocketClientExampleState extends State<RocketClientExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Rocket Example'),
+        title: const Text('Rocket Client Example'),
       ),
       body: Center(
         child: isLoading
