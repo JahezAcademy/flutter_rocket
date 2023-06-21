8d4f799be838b51a4fdfc1eea3be0c4188cd2802
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2023-06-21 10:40:09 -0700

    Added unit tests for RocketClient  (#58)
    * Added pot model for testing
    
    * Added unit tests for get cases
    
    * Added unit tests for get data without using model

--- packages/rocket_client/test/rocket_request_test.dart
+++ packages/rocket_client/test/rocket_request_test.dart
@@ -1,1 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rocket_client/rocket_client.dart';
+import 'package:rocket_model/rocket_model.dart';
 
+import 'post_model.dart';
+
+void main() {
+  group('RocketClient', () {
+    late RocketClient client;
+
+    setUp(() {
+      client = RocketClient(url: 'https://jsonplaceholder.typicode.com');
+    });
+
+    test('request - should get data using model from endpoint', () async {
+      final model = Post();
+      await client.request('posts/1', model: model);
+      expect(model.state, RocketState.done);
+      expect(model.id, 1);
+    });
+
+    test('request - should get list data using model from endpoint', () async {
+      final model = Post();
+      await client.request('posts', model: model);
+      expect(model.state, RocketState.done);
+      expect(model.all!.isNotEmpty, isTrue);
+    });
+
+    test('request - should handle errors', () async {
+      final model = Post();
+      await client.request('posts/invalid', model: model);
+      expect(model.state, RocketState.failed);
+      expect(model.exception, isA<RocketException>());
+      expect(model.exception.statusCode != 200, isTrue);
+    });
+
+    test('request - should get data from endpoint', () async {
+      var data =  await client.request('posts/1');
+      expect(data, isA<Map>());
+      expect(data['id'], 1);
+    });
+
+    test('request - should get list data from endpoint', () async {
+      var data = await client.request('posts');
+      expect(data, isA<List>());
+      expect(data.isNotEmpty, isTrue);
+    });
+  });
+}
