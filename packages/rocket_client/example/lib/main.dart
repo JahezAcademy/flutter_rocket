a629978f198bd189d20203de5f3fa9cdb851e3e4
Mohammed <m97.chahboun@gmail.com>
2025-02-24 09:24:01 +0000

    Update dependencies in pubspec.yaml and clean up example code

--- packages/rocket_client/example/lib/main.dart
+++ packages/rocket_client/example/lib/main.dart
@@ -106,8 +106,8 @@ class RocketClientExampleState extends State<RocketClientExample> {
     setState(() {});
     // Display the response in a dialog
     if (!isFailed) {
-      // ignore: use_build_context_synchronously
       showDialog(
+        // ignore: use_build_context_synchronously
         context: context,
         builder: (context) {
           return AlertDialog(
